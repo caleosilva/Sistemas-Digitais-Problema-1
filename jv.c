6648ee10db67ca043cc01dfd394c1c22aae48458
João Pedro <82676045+JPbast0z@users.noreply.github.com>
2024-04-12 17:23:16 -0300

    Update jv.c

--- jv.c
+++ jv.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-
 
 #include <fcntl.h>
 #include <pthread.h>
 
 #define LINHAS 3
 #define COLUNAS 3
+#define LIMITE_DESLOCAMENTO 5 // Define o limite mínimo de deslocamento necessário
 
 //* PARTE DO MOUSE INICIO
+
+// Jogador da vez
+char jogador = 'X';
+int playing = 1;
+
 // Variáveis globais para armazenar movimentos e cliques do mouse
+int raw_dx = 0;
+int raw_dy = 0;
 int dx = 0;
 int dy = 0;
 int botaoEsquerdo = 0;
@@ -25,74 +33,109 @@ int botaoEsquerdoAnterior = 0;
 int botaoDireitoAnterior = 0;
 int botaoMeioAnterior = 0;
 
-int linha = 2, coluna = 3;
+char tabuleiro[LINHAS][COLUNAS];
 
-void *monitorarMouse(void *arg) {
+// int linha = 2, coluna = 2;
+
+typedef struct
+{
+    int linha;
+    int coluna;
+    int rightClick;
+} InputMouse;
+
+void *monitorarMouse(void *arg)
+{
     int fd;
     char *mouse_device = "/dev/input/mice";
     signed char data[3];
 
     fd = open(mouse_device, O_RDONLY);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         perror("Erro ao abrir o dispositivo do mouse");
         return NULL;
     }
 
-    while(1) {
+    while (playing)
+    {
         read(fd, data, sizeof(data));
-
-        // Armazena os movimentos do mouse e o estado dos botões nas variáveis globais
-        dx = data[1];
-        dy = data[2];
+        raw_dx = data[1];
+        raw_dy = data[2];
         botaoEsquerdo = data[0] & 0x01;
         botaoDireito = data[0] & 0x02;
         botaoMeio = data[0] & 0x04;
 
-        // Intervalo de atualização
-        usleep(1500000); // 100 ms
+        dx = 0;
+        dy = 0;
+        
+        if (abs(raw_dx) >= abs(raw_dy)) {
+            if (raw_dx > LIMITE_DESLOCAMENTO) 
+                dx = 1;
+            else if (raw_dx < -LIMITE_DESLOCAMENTO)
+                dx = -1;
+        } else if (abs(raw_dy) > abs(raw_dx)) {
+            if (raw_dy > LIMITE_DESLOCAMENTO) 
+                dy = 1;
+            else if (raw_dy < -LIMITE_DESLOCAMENTO)
+                dy = -1;
+        }
     }
-
     close(fd);
     return NULL;
 }
 
-//* PARTE DO JOGO DA VELHA INICIO
-// Estrutura para representar o tabuleiro do jogo da velha
-char tabuleiro[LINHAS][COLUNAS];
-
 // Funcao para inicializar o tabuleiro do jogo da velha com espacos vazios
-void inicializar_tabuleiro() {
+void inicializar_tabuleiro()
+{
     int i, j;
-    for (i = 0; i < LINHAS; i++) {
-        for (j = 0; j < COLUNAS; j++) {
+    for (i = 0; i < LINHAS; i++)
+    {
+        for (j = 0; j < COLUNAS; j++)
+        {
             tabuleiro[i][j] = ' ';
         }
     }
 }
 
 // Funcao para desenhar o tabuleiro do jogo da velha
-void desenhar_tabuleiro() {
-    int i, j;
-    for (i = 0; i < LINHAS; i++) {
-        for (j = 0; j < COLUNAS; j++) {
-            printf(" %c ", tabuleiro[i][j]);
-            if (j < COLUNAS - 1)
-                printf("|");
+void *desenhar_tabuleiro(const InputMouse *data)
+{
+    while (playing)
+    {
+        if ((dx != dxAnterior) || (dy != dyAnterior) || (botaoEsquerdo != botaoEsquerdoAnterior))
+        {
+            system("clear");
+            printf("Linha X Coluna: %i -- %i\n", data->linha, data->coluna);
+
+            int i, j;
+            for (i = 0; i < LINHAS; i++)
+            {
+                for (j = 0; j < COLUNAS; j++)
+                {
+                    printf(" %c ", tabuleiro[i][j]);
+                    if (j < COLUNAS - 1)
+                        printf("|");
+                }
+                printf("\n");
+                if (i < LINHAS - 1)
+                    printf("---|---|---\n");
+            }
+            printf("\n");
         }
-        printf("\n");
-        if (i < LINHAS - 1)
-            printf("---|---|---\n");
     }
-    printf("\n");
 }
 
 // Funcao para colocar um simbolo (X ou O) em uma posicao especificada
-int colocar_simbolo(int linha, int coluna, char simbolo) {
-    if (linha < 0 || linha >= LINHAS || coluna < 0 || coluna >= COLUNAS) {
+int colocar_simbolo(int linha, int coluna, char simbolo)
+{
+    if (linha < 0 || linha >= LINHAS || coluna < 0 || coluna >= COLUNAS)
+    {
         printf("Posicao invalida!\n");
         return 0;
     }
-    if (tabuleiro[linha][coluna] != ' ') {
+    if (tabuleiro[linha][coluna] != ' ')
+    {
         printf("Posicao ja ocupada!\n");
         return 0;
     }
@@ -101,160 +144,190 @@ int colocar_simbolo(int linha, int coluna, char simbolo) {
 }
 
 // Funcao para verificar se algum jogador ganhou
-char verificar_vencedor() {
+char verificar_vencedor()
+{
     int i, j;
-
     // Verificar linhas e colunas
-    for (i = 0; i < LINHAS; i++) {
+    for (i = 0; i < LINHAS; i++)
+    {
         if (tabuleiro[i][0] != ' ' && tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][0] == tabuleiro[i][2])
             return tabuleiro[i][0];
         if (tabuleiro[0][i] != ' ' && tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[0][i] == tabuleiro[2][i])
             return tabuleiro[0][i];
     }
-
     // Verificar diagonais
     if (tabuleiro[0][0] != ' ' && tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[0][0] == tabuleiro[2][2])
         return tabuleiro[0][0];
     if (tabuleiro[0][2] != ' ' && tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[0][2] == tabuleiro[2][0])
         return tabuleiro[0][2];
-
     // Se nao houver vencedor
     return ' ';
 }
 
 // Funcao para verificar se houve um empate
-int verificar_empate() {
+int verificar_empate()
+{
     int i, j;
-
     // Verificar se todas as colulas estao preenchidas
-    for (i = 0; i < LINHAS; i++) {
-        for (j = 0; j < COLUNAS; j++) {
+    for (i = 0; i < LINHAS; i++)
+    {
+        for (j = 0; j < COLUNAS; j++)
+        {
             if (tabuleiro[i][j] == ' ')
                 return 0; // Ainda ha colunas vazias, o jogo nao estao empatado
         }
     }
-
     // Se todas as colunas estiverem preenchidas e nao houver vencedor, ha um empate
     return 1;
 }
 
-// Funcao para perguntar ao jogador em qual posicao ele deseja colocar seu simbolo
-void escolher_quadrante(char simbolo) {
+void definir_linha_coluna(InputMouse *data)
+{
+    int deslocamento_x = 0;
+    int deslocamento_y = 0;
 
+    if ((dx != dxAnterior) || (dy != dyAnterior))
+    {
+        dxAnterior = dx;
+        dyAnterior = dy;
+
+        if (abs(dx) > abs(dy))
+        {
+            if (dx > 0 && data->coluna < 3)
+            {
+                deslocamento_x = 1;
+            }
+            else if (dx < 0 && data->coluna > 1)
+            {
+                deslocamento_x = -1;
+            }
+
+            sleep(0.5);
+        }
+        else
+        {
+            if (dy > 0 && data->linha > 1)
+            {
+                deslocamento_y = -1;
+            }
+            else if (dy < 0 && data->linha < 3)
+            {
+                deslocamento_y = 1;
+            }
+
+            sleep(0.5);
+        }
+
+        data->linha += deslocamento_y;
+        data->coluna += deslocamento_x;
+    }
+}
+
+// Funcao para perguntar ao jogador em qual posicao ele deseja colocar seu simbolo
+void escolher_quadrante(InputMouse *data)
+{
     int jogada_feita = 0; // Variável para controlar se o jogador fez uma jogada
     int confirmar_jogada = 0;
 
-    while (!jogada_feita) {
+    if (botaoEsquerdo != botaoEsquerdoAnterior)
+    {
 
-        // Verifica se houve mudança nos valores do mouse desde a última impressão
-        if (dx != dxAnterior || dy != dyAnterior || botaoEsquerdo != botaoEsquerdoAnterior || botaoDireito != botaoDireitoAnterior || botaoMeio != botaoMeioAnterior) {
-            // Se houve mudança, atualiza os valores anteriores e imprime as informações do mouse
-            // Mostra as coordenadas atualizadas
-                printf("COORDENADAS: %i -- %i\n", linha, coluna);
+        if (botaoEsquerdo & 0x01)
+        {
+            confirmar_jogada = 1;
+        }
 
-            // Atualiza as coordenadas do quadrante com base no movimento do mouse
-            if (dy < 0 && linha > 1) {
-                linha--;
-            } else if (dy > 0 && linha < 3) {
-                linha++;
-            }
+        botaoEsquerdoAnterior = botaoEsquerdo;
 
-            else if (dx < 0 && coluna > 1) {
-                coluna--;
-            } else if (dx > 0 && coluna < 3) {
-                coluna++;
-            }
-            else if (botaoEsquerdo & 0x01){
-                confirmar_jogada = 1;
-            }
+        if (confirmar_jogada)
+        {
+            if (colocar_simbolo(data->linha - 1, data->coluna - 1, jogador))
+            {
+                jogada_feita = 1; // Define que o jogador fez uma jogada válida
 
-            dxAnterior = dx;
-            dyAnterior = dy;
-            botaoEsquerdoAnterior = botaoEsquerdo;
-            botaoDireitoAnterior = botaoDireito;
-            botaoMeioAnterior = botaoMeio;
-
-
-            if (confirmar_jogada == 1) {
-                // Verifica se o jogador fez uma jogada válida e sai do loop
-                if (colocar_simbolo(linha - 1, coluna - 1, simbolo)) {
-                    desenhar_tabuleiro();
-                    jogada_feita = 1; // Define que o jogador fez uma jogada válida
+                // Alternar entre X e O
+                if (jogador == 'X'){
+                    jogador = 'O';
+                    printf("Mudou");
                 }
-                confirmar_jogada = 0;
+                else {
+                    jogador = 'X';
+                }
             }
+            confirmar_jogada = 0;
         }
     }
 }
 
-
-
-
-
-
-int main() {
-
+int main()
+{
     pthread_t threadMouse;
+    pthread_t threadShowScreen;
 
-    if (pthread_create(&threadMouse, NULL, monitorarMouse, NULL)) {
+    InputMouse inputMouse = {2, 2, 0};
+
+    if (pthread_create(&threadMouse, NULL, monitorarMouse, NULL))
+    {
         fprintf(stderr, "Erro ao criar a thread do mouse\n");
         return 1;
     }
 
-    struct winsize w;
+    // if (pthread_create(&threadShowScreen, NULL, (void *(*)(void *))desenhar_tabuleiro, (void *)&inputMouse))
+    // {
+    //     fprintf(stderr, "Erro ao criar a thread dde apresentar a tela\n");
+    //     return 1;
+    // }
 
-    // Obter o tamanho do terminal
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-
-    // Largura e altura do terminal
-    int largura_terminal = w.ws_col;
-    int altura_terminal = w.ws_row;
-
-    // Calcular o tamanho da grade do jogo da velha
-    int tamanho_grade_largura = largura_terminal;
-    int tamanho_grade_altura = altura_terminal;
-
-    // Inicializar o tabuleiro do jogo da velha
     inicializar_tabuleiro();
 
-    // Desenhar o tabuleiro inicial
-    desenhar_tabuleiro();
-
-    // Variavel para alternar entre X e O
-    char jogador = 'X';
-
     // Loop principal do jogo
-    while (1) {
+    while (1)
+    {
+        definir_linha_coluna(&inputMouse);
+        escolher_quadrante(&inputMouse);
 
-
-
-
-        printf("Jogador %c, eh sua vez!\n", jogador);
-
-        escolher_quadrante(jogador);
-
-
-        // Verificar se algum jogador ganhou
         char vencedor = verificar_vencedor();
-        if (vencedor != ' ') {
+        if (vencedor != ' ')
+        {
+            system("clear");
             printf("Parabens! O jogador %c venceu!\n", vencedor);
+            playing = 0;
             break;
         }
-
-        // Verificar se houve um empate
-        if (verificar_empate()) {
+        if (verificar_empate())
+        {
+            system("clear");
             printf("Empate!\n");
+            playing = 0;
             break;
         }
 
-        // Alternar entre X e O
-        if (jogador == 'X')
-            jogador = 'O';
-        else
-            jogador = 'X';
-        // Outras tarefas do programa principal, se necessário
-        //usleep(50000); // 1 segundo
+        if (playing) {
+            if ((dx != dxAnterior) || (dy != dyAnterior) || (botaoEsquerdo != botaoEsquerdoAnterior))
+                {
+                    system("clear");
+                    printf("Linha X Coluna: %i -- %i\n", inputMouse.linha, inputMouse.coluna);
+
+                    int i, j;
+                    for (i = 0; i < LINHAS; i++)
+                    {
+                        for (j = 0; j < COLUNAS; j++)
+                        {
+                            printf(" %c ", tabuleiro[i][j]);
+                            if (j < COLUNAS - 1)
+                                printf("|");
+                        }
+                        printf("\n");
+                        if (i < LINHAS - 1)
+                            printf("---|---|---\n");
+                    }
+                    printf("\n");
+                }
+        }
     }
+
+    pthread_join(threadMouse, NULL);
+    pthread_join(threadShowScreen, NULL);
 
     return 0;
 }
